3d4ce475124999f74995d6c48defdddd7826c859
Saha <2146240@cognizant.com>
2024-01-12 13:08:29 +0530

    initial

---
+++ src/main/java/com/demo/ec2/Ec2Application.java
@@ -0,0 +1,24 @@
+package com.demo.ec2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+
+public class Ec2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec2Application.class, args);
+	}
+
+
+	@GetMapping("/hello")
+	public String helloWorld(){
+		return "Hello my World!!!";
+	}
+
+}
